fbdbf6201de60c32bff32060a814e261374bdf1f
ganha <milo.somera@gmail.com>
2020-09-28 22:56:50 +0800

    Updates

---
+++ precache-manifest.f49426300f5920fa30d2cf1ae68722ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7855e234bca0ae72f5800a23f426e6",
+    "url": "/socmed-pbexam/index.html"
+  },
+  {
+    "revision": "5acd796d2cf227a6f138",
+    "url": "/socmed-pbexam/static/css/main.8f4e51f2.chunk.css"
+  },
+  {
+    "revision": "5329e8fbead46e0235c7",
+    "url": "/socmed-pbexam/static/js/2.52b9ef13.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/socmed-pbexam/static/js/2.52b9ef13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5acd796d2cf227a6f138",
+    "url": "/socmed-pbexam/static/js/main.e2fcb429.chunk.js"
+  },
+  {
+    "revision": "8e8eb359c86087e54339",
+    "url": "/socmed-pbexam/static/js/runtime-main.4236d2cc.js"
+  }
+]);
